e8d0ee410badcaae0b1fcad53c544e3776b2cef4
sejal <710sejal@gmail.com>
2022-09-27 12:10:27 +0530

    romanNumberToNumber

---
+++ problem2.js
@@ -0,0 +1,33 @@
+
+// Change romanNumber into a Number
+
+function romanNumber(s){
+    let symbols = {
+    I:1,
+    V:5,
+    X:10,
+    L:50,
+    C:100,
+    D:500,
+    M:1000
+    };
+    let ans = 0;
+    for(let i = 0 ; i < s.length ; i++){
+      let curr = s[i] ;
+      let nxt = s[i+1] ;
+      if(symbols[curr]<symbols[nxt]){
+         ans = ans + symbols[nxt] - symbols[curr] ;
+          i++ ;
+        }
+     else{
+         ans = ans + symbols[curr];
+       }
+    }
+    return ans ;
+}
+
+let s ="XXVII";
+console.log(romanNumber(s));
+
+
+
