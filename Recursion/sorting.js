0cd3afb8f74619bf0bbe33b8fe9485dfac1df25e
Sejal Jaiswal <108399174+sejal710@users.noreply.github.com>
2023-03-15 06:39:46 +0000

    sorting help of recursion

---
+++ Recursion/sorting.js
@@ -0,0 +1,20 @@
+ 
+// Suppose an array is given find the sorted array
+
+function sort(arr,n){
+    if(n == 0 || n == 1){
+        return "Sorted Array"
+    }
+    if(arr[0] > arr[1]){
+        return "Oops Not Sorted"
+    }
+    else{ 
+    return sort(arr.shift(),n-1)
+    }
+}
+
+let mat = [1,2,4,7,9];
+console.log(sort(mat,mat.length));
+
+let arr = [2,3,6,1];
+console.log(sort(arr,arr.length))
